dcc0a6406cea70253a7a9503cac65c319cd0e738
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-01-29 17:38:10 -0500

    add imports for todo and project modules

--- src/projectModule.js
+++ src/projectModule.js
@@ -3,25 +3,29 @@ export class Project {
     constructor(name) {
         this.name = name;
         this.todoHolder = [];
+        console.log('project constructor has ran!');
+    }
+
+    test() {
+        console.log('project class has ran!');
     }
 }
 
-const projectManager = {
+export const projectManager = {
 
-    projectCollection = [],
+    projectCollection: [],
 
     add(name) {
         const newProject = new Project(name);
         this.projectCollection.push(newProject);
+        console.log('add method ran!')
         return newProject;   
     },
 
     remove(name) {
         const index = this.projectCollection.findIndex(project => project.name === name);
         this.projectCollection.splice(index, 1);
+        console.log('remove method ran!');
     },
 
-    get(project) {
-
-    }
 }
