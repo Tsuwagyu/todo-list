a9a5ebf36a593988c798f75e1775f0dc7d6c0351
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-02-03 14:00:16 -0500

    add save and load functions to work with localStorage

--- src/index.js
+++ src/index.js
@@ -8,3 +8,18 @@ const milk = new todo('Get milk', 'from the store', '2024-01-15', 'low', '');
 inbox.todoHolder.push(milk);
 
 console.log(inbox);
+
+
+function save(){
+    let projectManagerRef = projectManager.projectCollection
+    JSON.stringify(projectManagerRef);
+    localStorage.setItem('projectManagerRef', jsonString);
+}
+
+function load() {
+    localStorage.getItem('projectManagerRef');
+    JSON.parse(projectManagerRef);
+}
+
+
+
