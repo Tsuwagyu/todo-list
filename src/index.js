d9e63fe54c1c0377426dd1e68a34362a4b2c6ae1
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-02-03 15:32:12 -0500

    refactor: index.js wiil not handle storage related workflow

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import { todo } from "./todoModule.js";
 import { Project } from "./projectModule.js";
 import { projectManager } from "./projectModule.js";
-
-// call projectManager for inbox default
-const inbox = projectManager.add('Inbox');
-const milk = new todo('Get milk', 'from the store', '2024-01-15', 'low', '');
-inbox.todoHolder.push(milk);
-
-console.log(inbox);
+import { storage } from "./storageModule.js";
 
 
-function save(){
-    let projectManagerRef = projectManager.projectCollection
-    JSON.stringify(projectManagerRef);
-    localStorage.setItem('projectManagerRef', jsonString);
+function saveStuff() {
+    const savedData = projectManager.projectCollection;
 }
-
-function load() {
-    localStorage.getItem('projectManagerRef');
-    JSON.parse(projectManagerRef);
-}
-
-
-
