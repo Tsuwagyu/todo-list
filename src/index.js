4b78bb0e8e615f8a3d44027da493356a713c1468
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-02-05 12:36:28 -0500

    add test cases to check storage and dom modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { todo } from "./todoModule.js";
 import { Project, projectManager } from "./projectModule.js";
 import { storage } from "./storageModule.js";
+import { domManager } from "./domModule.js";
+
+
 
 const loadedProjects = storage.load();
 
@@ -12,3 +15,14 @@ if (loadedProjects.length > 0) {
     console.log("No data found, initializing default inbox.");
     projectManager.add('Inbox');
 }
+
+// const inbox = projectManager.add('Inbox');
+// const work = projectManager.add('Work');
+// const milk = new todo('Get milk', 'from store', '2024-01-15', 'low', '');
+// inbox.todoHolder.push(milk);
+
+storage.save(projectManager.projectCollection);
+console.log(projectManager.projectCollection);
+localStorage.clear();
+
+domManager.renderProjects();
