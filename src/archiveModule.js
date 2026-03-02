f7412a3091e7e5b7a20d31d90fddbcf20203bf2d
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-03-02 11:55:20 -0500

    add module for handling completed tasks

---
+++ src/archiveModule.js
@@ -0,0 +1,32 @@
+import { storage } from "./storageModule.js";
+import { Project, projectManager } from "./projectModule.js";
+
+export const archiveManager = {
+
+    archivedTasks: [],
+
+    addToArchive(task) {
+        this.archivedTasks.push(task);
+    },
+
+    removeFromArchive(id) {
+
+        const index = this.archivedTasks.findIndex(task => task.id === id);
+        this.archivedTasks.splice(index, 1);
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
